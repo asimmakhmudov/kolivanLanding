f9e042b007a8c614fb42767e99addbd780dda82e
asimmakhmudov <asimmahmudov18@gmail.com>
2023-09-09 00:04:13 +0400

    update

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -14,7 +14,7 @@ const Intro = () => {
       <p>{t('intro.content')}</p>
       <div className='linktoproduct'>
         <button>
-          <p>{t('intro.start')}</p>
+          {t('intro.start')}
         </button>
       </div>
       <img src={scrollAnimation} alt="scrollMouse" className='scrollMouse' />
